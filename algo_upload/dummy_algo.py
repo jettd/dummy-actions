6f8cc8cd6848dd327f02298258ce5d5b588471d3
Jett Durante <jdurante2@MacBook-Pro.local>
2023-09-22 12:28:21 -0500

    first action test

---
+++ algo_upload/dummy_algo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import random
+
+randnums = [random.randint(1, 100) for _ in range(10)]
+
+with open("random_output.txt", "w") as f:
+    for num in randnums:
+        f.write(str(num) + '\n')
+
+print("10 random nums")
+
+if (randnums[0] % 10):
+    exit randnums[1]
+
+exit 0
+
+
--- algo_upload/dummy_algo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "algo executed"
-exit 0
