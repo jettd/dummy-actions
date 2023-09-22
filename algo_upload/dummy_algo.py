10683a2c5a08bd63b889bef4246192a33399c998
Jett Durante <jdurante2@MacBook-Pro.local>
2023-09-22 12:55:34 -0500

    action test 9

--- algo_upload/dummy_algo.py
+++ algo_upload/dummy_algo.py
@@ -10,8 +10,8 @@ with open("random_output.txt", "w") as f:
 
 print("10 random numbers.")
 
-if (randnums[0] == 99):
-    exit (randnums[0])
+# if (randnums[0] == 99): 
+#     exit (randnums[0])
 
 exit (0)
 
