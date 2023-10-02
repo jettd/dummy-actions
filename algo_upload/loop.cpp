dd1cf526158270bfff90dd230f38f5e27d8bfe3a
Jett Durante <jdurante2@MacBook-Pro-61.local>
2023-10-02 10:48:42 -0500

    lang 3 cpp

--- algo_upload/loop.cpp
+++ algo_upload/loop.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 10; i++) {
         std::cout << "Iteration number: " << i << std::endl;
     }
     return 0;
