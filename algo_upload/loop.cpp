680548502f31db712b7a0e81c758c0aaebde36b5
Jett Durante <jdurante2@MacBook-Pro-61.local>
2023-10-02 11:02:40 -0500

    commit test 1/2

--- algo_upload/loop.cpp
+++ algo_upload/loop.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 5; i++) {
         std::cout << "Iteration number: " << i << std::endl;
     }
     return 0;
