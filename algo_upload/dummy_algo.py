d713ce52b0edeff34f4c506f5eba8ba98f682c71
Jett Durante <jdurante2@macbook-pro-61.unl.edu>
2023-10-05 11:31:16 -0500

    a

--- algo_upload/dummy_algo.py
+++ algo_upload/dummy_algo.py
@@ -2,13 +2,13 @@
 
 import random
 
-randnums = [random.randint(1, 1000) for _ in range(10)]
+randnums = [random.randint(1, 1000) for _ in range(11)]
 
 with open("random_output.txt", "w") as f:
     for num in randnums:
         f.write(str(num) + '\n')
 
-print("10 random numbers written to random_output")
+print("11 random numbers written to random_output")
 exit (0)
 
 
